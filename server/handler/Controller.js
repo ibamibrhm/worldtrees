be903477f6e34b458ff53521cc55542f5acbb8fc
ibamibrhm <ibrhm.ibam@gmail.com>
2021-02-23 00:16:18 +0700

    add UTC_TIMESTAMP

--- server/handler/Controller.js
+++ server/handler/Controller.js
@@ -21,7 +21,7 @@ module.exports = {
        * treeCutPerSecond = Math.round(marginTreePerYear / 365 / 86400)
        * treeCutperminute = treeCutPerSecond: 317 * 60 => 19020
       */
-      await prisma.$executeRaw('UPDATE tree SET total_tree = total_tree - 19020 ORDER BY id DESC LIMIT 1');
+      await prisma.$executeRaw('UPDATE tree SET total_tree = total_tree - 19020, updated_at = UTC_TIMESTAMP ORDER BY id DESC LIMIT 1');
       this.getTree(socket);
     } catch (error) {
       console.log(error);
